c3b67094f3dc3a55a710197a9ef41c204525cb52
Jeferson-Peter <jefersonpeter32@gmail.com>
2023-02-12 17:02:44 -0300

    Update setup.py with new 0.0.7 version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     LONG_DESCRIPTION = "\n" + fh.read()
 
-VERSION = '0.0.6'
+VERSION = '0.0.7'
 DESCRIPTION = 'A basic dynamic class to support I18N translation from JSON'
 
 project_urls = {
